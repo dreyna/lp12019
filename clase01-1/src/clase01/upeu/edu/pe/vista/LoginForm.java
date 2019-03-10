050c7d214e3047035fce59d53ad36171a54622c8
David Reyna <David Reyna@DESKTOP-53NO1KM>
2019-03-09 22:14:28 -0500

    v5

--- clase01-1/src/clase01/upeu/edu/pe/vista/LoginForm.java
+++ clase01-1/src/clase01/upeu/edu/pe/vista/LoginForm.java
@@ -23,6 +23,8 @@ import java.awt.Toolkit;
 import javax.swing.ImageIcon;
 import javax.swing.SwingConstants;
 import java.awt.Font;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 @SuppressWarnings("serial")
 public class LoginForm extends JFrame {
@@ -72,13 +74,36 @@ public class LoginForm extends JFrame {
 		lblNewLabel.setBounds(15, 277, 24, 24);
 		panel.add(lblNewLabel);
 		txtusuario = new JTextField();
+		txtusuario.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent evt) {
+				if(evt.getKeyCode() == KeyEvent.VK_ENTER){
+					txtpassword.requestFocus();
+				}
+			}
+		});
 		txtusuario.setHorizontalAlignment(SwingConstants.CENTER);
 		txtusuario.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		txtusuario.setBounds(49, 277, 180, 40);
 		panel.add(txtusuario);
 		txtusuario.setColumns(10);
-		
+		JButton btnenviar = new JButton("Login");
+		btnenviar.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				registrar();
+			}
+		});
 		txtpassword = new JPasswordField();
+		txtpassword.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent evt) {
+				if(evt.getKeyCode() == KeyEvent.VK_ENTER){
+					btnenviar.requestFocus();
+					}
+				}
+			
+		});
 		txtpassword.setHorizontalAlignment(SwingConstants.CENTER);
 		txtpassword.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		txtpassword.setBounds(49, 326, 180, 40);
@@ -87,9 +112,8 @@ public class LoginForm extends JFrame {
 		JLabel lblPassword = new JLabel("");
 		lblPassword.setIcon(new ImageIcon(LoginForm.class.getResource("/imagenes/Key_24x24.png")));
 		lblPassword.setBounds(10, 326, 29, 33);
-		panel.add(lblPassword);
+		panel.add(lblPassword);		
 		
-		JButton btnenviar = new JButton("Login");
 		btnenviar.setHorizontalTextPosition(SwingConstants.LEFT);
 		btnenviar.setIcon(new ImageIcon(LoginForm.class.getResource("/imagenes/Right3Green.png")));
 		btnenviar.setFont(new Font("Tahoma", Font.PLAIN, 14));
@@ -103,30 +127,32 @@ public class LoginForm extends JFrame {
 		panel.add(label);		
 		btnenviar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				try {
-					String user = txtusuario.getText();
-					String pass = new String(txtpassword.getPassword());
-					if(user.equals("") || pass.equals("")) {
-						JOptionPane.showMessageDialog(null,"Ingresar Datos");
-					}else {
-						if(dao.validar(new Usuario(user,pass))==1) {
-							PrincipalForm pf = new PrincipalForm();
-							pf.setVisible(true);
-							setVisible(false);
-							System.out.println();
-						}else {
-							JOptionPane.showMessageDialog(null,"Datos Incorrectos...!");							
-						}
-					}
-				   
-				} catch (Exception e) {
-					// TODO: handle exception
-					JOptionPane.showMessageDialog(null,"Datas Incorrectos");
-				}
-				
-			
+				registrar();
 			}
 			
 		});
+		
+	}
+	public void registrar() {
+		try {
+			String user = txtusuario.getText();
+			String pass = new String(txtpassword.getPassword());
+			if(user.equals("") || pass.equals("")) {
+				JOptionPane.showMessageDialog(null,"Ingresar Datos");
+			}else {
+				if(dao.validar(new Usuario(user,pass))==1) {
+					PrincipalForm pf = new PrincipalForm();
+					pf.setVisible(true);
+					setVisible(false);
+					System.out.println();
+				}else {
+					JOptionPane.showMessageDialog(null,"Datos Incorrectos...!");							
+				}
+			}
+		   
+		} catch (Exception e) {
+			// TODO: handle exception
+			JOptionPane.showMessageDialog(null,"Datas Incorrectos");
+		}
 	}
 }
